d98c95c9c7c7662239ffc773d91838feb58f94cf
root <root@HOME-PC.>
2025-04-27 16:16:16 +0000

    add needed fields to user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,27 +1,30 @@
 class RegistrationsController < Devise::RegistrationsController
-  clear_respond_to
   respond_to :json
-
-  def create
-    build_resource(sign_up_params)
-    
-    resource.save
-    if resource.persisted?
-      render json: {
-        status: { code: 200, message: 'Signed up successfully' },
-        data: { email: resource.email, id: resource.id }
-      }
-    else
-      render json: {
-        status: { code: 422, message: "User couldn't be created", 
-                errors: resource.errors.full_messages }
-      }, status: :unprocessable_entity
-    end
-  end
 
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, 
+                                :first_name, :last_name, :role)
+  end
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message: 'Signed up successfully' },
+        user: {
+          id: resource.id,
+          email: resource.email,
+          first_name: resource.first_name,
+          last_name: resource.last_name,
+          role: resource.role
+        }
+      }
+    else
+      render json: {
+        status: { message: "User couldn't be created", 
+                 errors: resource.errors.full_messages }
+      }, status: :unprocessable_entity
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,15 @@ class SessionsController < Devise::SessionsController
   
     def respond_with(resource, _opts = {})
       render json: {
-        status: { code: 200, message: 'Logged in successfully' },
-        user: {
-    email: resource.email,
-    id: resource.id
-  },
-  token: request.env['warden-jwt_auth.token'] || resource.generate_jwt_token
+          status: { code: 200, message: 'Logged in successfully' },
+          user: {
+        id: resource.id,
+        email: resource.email,
+        first_name: resource.first_name,
+        last_name: resource.last_name,
+        role: resource.role
+      },
+        token: request.env['warden-jwt_auth.token'] || resource.generate_jwt_token
       }
     end
   
